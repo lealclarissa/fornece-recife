c1eb1552cff21473600c1fe79c720f30f056d336
lealclarissa <lealclarissa2@gmail.com>
2020-12-09 23:40:43 -0300

    add server and app

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(`${process.env.MONGODB_URL}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
