bb1cd87bbfee95470ed602eac7f0df6811825ee4
lealclarissa <lealclarissa2@gmail.com>
2020-12-12 00:28:43 -0300

    update index

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,10 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.status(200).send({
     title: "API - Fornecedores de Alimentos e Bebidas - Recife",
-    version: "1.0.0",
+    version: "1.1.0",
     label: "Projeto de conclusão de curso",
     turma: "on7 Backend Minas",
+    owner: "github.com/lealclarissa"
   });
 });
 
